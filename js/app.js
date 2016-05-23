64d44cf2692c6e4bcc0f0ee35e07f0bfb1597391
ktw016 <ktw016@gmail.com>
2016-05-23 01:58:26 -0700

    street fighter challenge

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
-    $('.ryu').mouseenter(function(){
-        $('.ryu-still').hide();
+    $('.ryu, .hulk-ryu').mouseenter(function(){
+        $('.ryu-action').hide();
         $('.ryu-ready').show();
     })
+
     .mouseleave(function() {
+        $('.ryu-action').hide();
         $('.ryu-still').show();
-        $('.ryu-ready').hide();
     })
+
     .mousedown(function(){
         playHadouken();
-        $('.ryu-ready').hide();
+        $('.ryu-action').hide();
         $('.ryu-throwing').show();
         $('.hadouken').finish().show().animate(
             {'left': '1020px'},
@@ -18,23 +20,35 @@ $(document).ready(function(){
                 $(this).hide();
                 $(this).css('left', '520px');
             });
+        $('.evil-hadouken').finish().show().animate(
+            {'right': '1020px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('right', '520px');
+            });
+
     })
+
     .mouseup(function(){
-        $('.ryu-throwing').hide();
+        $('.ryu-action').hide();
         $('.ryu-ready').show();
-    })
+    });
+
     $(document).keydown(function(e){
-    	if(e.keyCode == 88){
-    		$('.ryu-still').hide();
-    		$('.ryu-cool').show();
+    	if(e.keyCode === 88){
+    		$('.ryu-action').hide();
+            $('.ryu-cool').show();
     	}
     })
+ 
     .keyup(function(e){
-    	if(e.keyCode == 88){
-    		$('.ryu-still').show();
+    	if(e.keyCode === 88){
     		$('.ryu-cool').hide();
+            $('.ryu-still').show();
     	}
     });
+
 
 });
 
@@ -43,3 +57,4 @@ function playHadouken() {
     $('#hadouken-sound')[0].load();
     $('#hadouken-sound')[0].play();
 }
+
